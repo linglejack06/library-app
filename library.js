91f902c3fbcb66a572eab46e9e3e76085e5c4c0f
Jack Lingle <captainjack0818@gmail.com>
2023-03-26 14:29:31 -0400

    Add event listeners to close and open form popup

--- library.js
+++ library.js
@@ -1,27 +1,40 @@
-/* eslint-disable no-multi-assign */
+
 const myLibrary = [];
 const library = document.querySelector('.library');
+const form = document.querySelector('.popup-form');
+const formBtn = document.getElementById('add-book');
+const closeFormBtn = document.querySelector('.cancel-btn');
+closeFormBtn.addEventListener('click', () => {
+    form.style.display = 'none';
+})
+formBtn.addEventListener('click', () => {
+    form.style.display = 'block';
+});
+
+// constructor function
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
-
+// add book to library
 function addBook() {
     
 }
+// make card
 function createBookCard(book) {
     const card = document.createElement('div');
     const bookTitle = document.createElement('h3') ;
     bookTitle.textContent = book.title;
-    const bookAuthor = document.createElement('h3');
+    const bookAuthor = document.createElement('h4');
     bookAuthor.textContent = `By ${book.author}`;
     const bookPages = document.createElement('p');
     bookPages.textContent = `${book.pages} Pages`;
     card.appendChild(bookTitle);
     card.appendChild(bookAuthor);
     card.appendChild(bookPages);
+    card.classList.add('book-card');
     library.appendChild(card)
 }
 const book = new Book('The Hobbit', 'C.S Lewis', 400, false);
