a1ae3c16c60e9dfc45898386d17c61a0f7f29b24
Jack Lingle <captainjack0818@gmail.com>
2023-03-27 12:35:48 -0400

    Add toggle button for read

--- library.js
+++ library.js
@@ -6,7 +6,7 @@ const formBtn = document.getElementById('add-book');
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const pageInput = document.getElementById('pages');
-const isRead = document.getElementById('read');
+const isRead = document.getElementById('read-status');
 const closeFormBtn = document.querySelector('.cancel-btn');
 
 // MARK: book constructor + functions
@@ -18,23 +18,42 @@ function Book(title, author, pages, read) {
 }
 Book.prototype.createBookCard = function createBookCard() {
     const card = document.createElement('div');
+
     const bookTitle = document.createElement('h3') ;
     bookTitle.textContent = this.title;
+
     const bookAuthor = document.createElement('h4');
     bookAuthor.textContent = `By ${this.author}`;
+
     const bookPages = document.createElement('p');
     bookPages.textContent = `${this.pages} Pages`;
+
     const deleteButton = document.createElement('button');
     deleteButton.type = 'button';
     deleteButton.textContent = 'Delete';
-    deleteButton.classList.add('delete-btn');
+    deleteButton.classList.add('red-btn');
     deleteButton.addEventListener('click', () => {
         library.removeChild(card);
     });
+
+    const isReadButton = document.createElement('button');
+    isReadButton.type = 'button';
+    isReadButton.textContent = this.read === '1' ? 'read' : 'not read';
+    isReadButton.classList.add(this.read === '1' ? 'green-btn' : 'red-btn');
+    isReadButton.addEventListener('click', () => {
+        if(this.read === '1') {
+            this.read = '0';
+        } else {
+            this.read = '1';
+        }
+        isReadButton.textContent = this.read === '1' ? 'Read' : 'Not Read';
+        isReadButton.classList.add(this.read === '1' ? 'green-btn' : 'red-btn');
+    })
     card.appendChild(bookTitle);
     card.appendChild(bookAuthor);
     card.appendChild(bookPages);
     card.appendChild(deleteButton);
+    card.appendChild(isReadButton);
     card.classList.add('book-card');
     card.dataset.index = myLibrary.indexOf(this);
     library.appendChild(card);
@@ -49,7 +68,7 @@ Book.prototype.addToLibrary = function addToLibrary() {
 function addBook(e) {
     // prevent form from trying to submit to server-side
     e.preventDefault();
-    const book = new Book(titleInput.value, authorInput.value, pageInput.value, isRead);
+    const book = new Book(titleInput.value, authorInput.value, pageInput.value, isRead.value);
     book.addToLibrary();
     book.createBookCard();
 }
@@ -63,5 +82,5 @@ closeFormBtn.addEventListener('click', () => {
 formBtn.addEventListener('click', () => {
     form.style.display = 'block';
 });
-const book = new Book('The Hobbit', 'C.S Lewis', 400, false);
+const book = new Book('The Hobbit', 'C.S Lewis', 400, '1');
 book.createBookCard();
