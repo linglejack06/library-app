6fe9541bc7d491f442e2ea6bdd5e26e3d5f81e01
Jack Lingle <captainjack0818@gmail.com>
2023-04-06 11:52:45 -0400

    Convert object literal model to class model

--- library.js
+++ library.js
@@ -12,64 +12,62 @@ const isRead = document.getElementById('read-status');
 const closeFormBtn = document.querySelector('.cancel-btn');
 let bookNumber = 0;
 
-// MARK: book constructor + functions
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-Book.prototype.createBookCard = function createBookCard() {
-    const card = document.createElement('div');
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-    const bookTitle = document.createElement('h3') ;
-    bookTitle.textContent = this.title;
+    createBookCard() {
+        const card = document.createElement('div');
+        const bookTitle = document.createElement('h3') ;
+        bookTitle.textContent = this.title;
+        const bookAuthor = document.createElement('h4');
+        bookAuthor.textContent = `By ${this.author}`;
+        const bookPages = document.createElement('p');
+        bookPages.textContent = `${this.pages} Pages`;
+        const deleteButton = document.createElement('button');
+        deleteButton.type = 'button';
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('red-btn');
+        deleteButton.addEventListener('click', () => {
+            library.removeChild(card);
+            bookNumber -= 1;
+            booksRead.textContent = `${bookNumber} Books Read`
+        });
 
-    const bookAuthor = document.createElement('h4');
-    bookAuthor.textContent = `By ${this.author}`;
-
-    const bookPages = document.createElement('p');
-    bookPages.textContent = `${this.pages} Pages`;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.type = 'button';
-    deleteButton.textContent = 'Delete';
-    deleteButton.classList.add('red-btn');
-    deleteButton.addEventListener('click', () => {
-        library.removeChild(card);
-        bookNumber -= 1;
-        booksRead.textContent = `${bookNumber} Books Read`
-    });
-
-    const isReadButton = document.createElement('button');
-    isReadButton.type = 'button';
-    isReadButton.textContent = this.read === '1' ? 'read' : 'not read';
-    isReadButton.classList.add(this.read === '1' ? 'green-btn' : 'red-btn');
-    isReadButton.addEventListener('click', () => {
-        if(this.read === '1') {
-            this.read = '0';
-        } else {
-            this.read = '1';
-        }
-        isReadButton.textContent = this.read === '1' ? 'Read' : 'Not Read';
+        const isReadButton = document.createElement('button');
+        isReadButton.type = 'button';
+        isReadButton.textContent = this.read === '1' ? 'read' : 'not read';
         isReadButton.classList.add(this.read === '1' ? 'green-btn' : 'red-btn');
-    })
-    card.appendChild(bookTitle);
-    card.appendChild(bookAuthor);
-    card.appendChild(bookPages);
-    card.appendChild(isReadButton);
-    card.appendChild(deleteButton);
-    card.classList.add('book-card');
-    card.dataset.index = myLibrary.indexOf(this);
-    library.appendChild(card);
+        isReadButton.addEventListener('click', () => {
+            if(this.read === '1') {
+                this.read = '0';
+            } else {
+                this.read = '1';
+            }
+            isReadButton.textContent = this.read === '1' ? 'Read' : 'Not Read';
+            isReadButton.classList.add(this.read === '1' ? 'green-btn' : 'red-btn');
+        })
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(isReadButton);
+        card.appendChild(deleteButton);
+        card.classList.add('book-card');
+        card.dataset.index = myLibrary.indexOf(this);
+        library.appendChild(card);
+    }
+
+    addToLibrary() {
+        myLibrary.push(this);
+        bookNumber += 1;
+        booksRead.textContent = `${bookNumber} Books Read`;
+    }
 }
 
-Book.prototype.addToLibrary = function addToLibrary() {
-    // adds this(book) to array
-    myLibrary.push(this);
-    bookNumber += 1;
-    booksRead.textContent = `${bookNumber} Books Read`;
-}
 
 
 function addBook(e) {
